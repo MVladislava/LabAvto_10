29783c797acb706e980571867d1a599298081f63
S1L3N7R4T <vsmyasnikova@edu.hse.ru>
2024-02-23 18:52:32 +0500

    Добавьте файлы проекта.

---
+++ LabAvto_10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Cars;
+namespace LabaAvto_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Show();
+            LightCar lightCar = new LightCar(2,300);
+            lightCar.Show();
+            OffRoad offRoad = new OffRoad();
+            offRoad.Show();
+            Truck truck = new Truck();
+            truck.Show();
+        }
+    }
+}
